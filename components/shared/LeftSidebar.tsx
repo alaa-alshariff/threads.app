60dc6c15b85bbd557e212441d3c54eaafe4835e2
Alaa-Alsharif <zoma_alaa@live.com>
2023-12-15 23:41:58 -0500

    first commit

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+function LeftSidebar() {
+  const Router = useRouter();
+  const Pathname = usePathname();
+  const { userId } = useAuth();
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (Pathname.includes(link.route) && link.route.length > 1) ||
+            Pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"} `}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden"> {link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => Router.push("/sign-in")}>
+            <div className="flex cursor-pointer">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden px-3">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
+}
+
+export default LeftSidebar;

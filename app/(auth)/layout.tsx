c23eb22a36f87ce42940b11fa923e81a6a69e66e
alaa-alshariff <alaa.alshariff@hotmail.com>
2023-12-16 17:43:12 -0500

    Done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1 flex flex-1 w-full justify-center align-middle`}>{children}</body>
       </html>
     </ClerkProvider>
   );
